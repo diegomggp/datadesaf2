b728473ea754aabbb55d4096b7feb253d7a7113d
marcosmarinalopez <marcosmarinalopez@outlook.es>
2023-08-12 00:04:55 +0200

    api/v1/metrics tested with test JSON

--- SpotifyRecommender.py
+++ SpotifyRecommender.py
@@ -13,13 +13,22 @@ def home():
 
 @app.route('/api/v1/metrics', methods=['POST'])
 def metrics():
+    '''
+    API call  to predict the metrics for next Spotify songs with the provided JSON about other songs metrics
+
+    Args:
+        json (JSON): JSON data with the songs metrics
+
+    Returns:
+        Dictionary: A dictionary with the metrics to use for retrieving information from Spotify
+    '''
     try:
         
         if request.is_json:
             data = request.get_json()      
             analyzer = MetricAnalyzer('metric_analyzer')
             metrics = analyzer.fx__get_metrics(data)
-            print(metrics)
+            # print(metrics)
             json_result =  jsonify(metrics)
         else:
             json_result = jsonify({"message": "El contenido de la petición no es un JSON"}), 400
